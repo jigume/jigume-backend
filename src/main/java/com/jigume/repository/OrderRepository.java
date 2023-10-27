21eaa8578c9cb813b8f83b40091c46ef69553cef
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-10-27 22:49:17 +0900

    fix: 구현하지 않은 리포지터리 메서드 구현

--- src/main/java/com/jigume/repository/OrderRepository.java
+++ src/main/java/com/jigume/repository/OrderRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    List<Order> findOrdersByGoodsId(Long goodsId);
+    List<Order> findOrdersByMemberId(Long memberId);
 
 }
