25f0b8049d27aba1046ac8532db74a65b30b065d
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2024-01-01 18:30:00 +0900

    refactor: GoodsStatus 숫자 제거

--- src/main/java/com/jigume/domain/goods/entity/GoodsStatus.java
+++ src/main/java/com/jigume/domain/goods/entity/GoodsStatus.java
@@ -4,17 +4,5 @@ import lombok.Getter;
 
 @Getter
 public enum GoodsStatus {
-    PROCESSING(0), END(1);
-
-    private final Integer status;
-
-    GoodsStatus(Integer status) {
-        this.status = status;
-    }
-
-//    public static GoodsStatus getGoodsStatus(Integer status) {
-//        return Arrays.stream(GoodsStatus.values()).filter(goodsStatus -> goodsStatus.getStatus() == status)
-//                .findFirst()
-//                .orElseThrow(() -> new ());
-//    }
+    PROCESSING, END;
 }
