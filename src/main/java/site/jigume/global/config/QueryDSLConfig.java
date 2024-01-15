23f65cff620c112e0dc01a3b51d4fe3e9dfc071a
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2024-01-15 21:15:35 +0900

    config: QueryDSL Config 설정

---
+++ src/main/java/site/jigume/global/config/QueryDSLConfig.java
@@ -0,0 +1,19 @@
+package site.jigume.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDSLConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
