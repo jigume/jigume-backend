cfd76c0a28041f5498450bf3b8eb400b68d3d74d
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2024-02-12 21:55:41 +0900

    feat: 삭제 가능 기준 추가

--- src/main/java/site/jigume/domain/goods/service/commend/GoodsDeleteService.java
+++ src/main/java/site/jigume/domain/goods/service/commend/GoodsDeleteService.java
@@ -3,16 +3,14 @@ package site.jigume.domain.goods.service.commend;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import site.jigume.domain.board.repository.BoardRepository;
 import site.jigume.domain.goods.entity.Goods;
+import site.jigume.domain.goods.entity.GoodsStatus;
 import site.jigume.domain.goods.exception.GoodsException;
-import site.jigume.domain.goods.repository.GoodsImagesRepository;
 import site.jigume.domain.goods.repository.GoodsRepository;
 import site.jigume.domain.member.entity.Member;
 import site.jigume.domain.member.exception.auth.AuthException;
 import site.jigume.domain.member.exception.auth.AuthExceptionCode;
 import site.jigume.domain.member.service.MemberService;
-import site.jigume.domain.order.repository.SellRepository;
 
 import static site.jigume.domain.goods.exception.GoodsExceptionCode.GOODS_DELETE_IMPOSSIBLE;
 import static site.jigume.domain.goods.exception.GoodsExceptionCode.GOODS_NOT_FOUND;
@@ -32,9 +30,11 @@ public class GoodsDeleteService {
 
         checkGoodsSeller(goods, member);
 
-        if (goods.getOrderList().size() > 0) {
+        if (goods.getOrderList().size() > 0 || goods.getGoodsStatus() != GoodsStatus.FINISHED) {
             throw new GoodsException(GOODS_DELETE_IMPOSSIBLE);
         }
+
+        goods.updateEnd();
 
         goods.delete();
         goods.getBoard().delete();
